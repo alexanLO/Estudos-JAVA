f2a616f5794270f3210f64636c8683833480695b
Alexan Lima <alexan.works@gmail.com>
2022-02-10 13:15:48 -0300

    Limitações para TextField, interface Inicializable

--- Interface_Grafica_JAVAFX/src/gui/ViewController.java
+++ Interface_Grafica_JAVAFX/src/gui/ViewController.java
@@ -1,13 +1,17 @@
 package gui;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ViewController {
+public class ViewController implements Initializable{
 
     @FXML
     private TextField txtNumberOne;
@@ -28,4 +32,14 @@ public class ViewController {
         double sum = numberOne + numberTwo;
         labelResult.setText(String.format(Locale.US, "%.2f", sum));
     }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle rb) {
+        Constraints.setTextFieldDouble(txtNumberOne);
+        Constraints.setTextFieldDouble(txtNumberTwo);
+        Constraints.setTextFieldMaxLength(txtNumberOne, 12);
+        Constraints.setTextFieldMaxLength(txtNumberTwo, 12);
+        
+    }
+
 }
